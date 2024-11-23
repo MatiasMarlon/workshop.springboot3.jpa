80aaa0ea0b4d03fa4681805f5ef855f1fc256d39
Marlon Henrique <matiasmarlon2003@gmail.com>
2024-11-23 10:20:30 -0300

    Informaçoes adicionais

--- src/main/java/com/udemy/course/resources/UserResource.java
+++ src/main/java/com/udemy/course/resources/UserResource.java
@@ -9,6 +9,15 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.udemy.course.entities.User;
 
+/* @RestController: Responsável por lidar com requisições HTTP e retornar respostas, geralmente no formato JSON.
+ *
+ * @RequestMapping(value = "/users"): Define o caminho base (base path) para os endpoints da classe. Todas as requisições HTTP 
+ * que começarem com "/users" serão direcionadas para os métodos dessa classe.
+ * 
+ * @GetMapping: Mapeia um método de um controlador para uma requisição HTTP GET. Ou seja, quando uma requisição HTTP GET for 
+ * feita para o endpoint especificado, esse método será executado.
+ */
+
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
