86344e0b9dddf92dd119628d4fe962da014ad0b2
Marlon Henrique <matiasmarlon2003@gmail.com>
2024-11-23 15:14:39 -0300

    H2 database, test profile, JPA

--- src/main/java/com/udemy/course/resources/UserResource.java
+++ src/main/java/com/udemy/course/resources/UserResource.java
@@ -1,7 +1,7 @@
 package com.udemy.course.resources;
 
+import java.util.ArrayList;
 import java.util.List;
-
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,13 +10,13 @@ import org.springframework.web.bind.annotation.RestController;
 import com.udemy.course.entities.User;
 
 /* @RestController: Responsável por lidar com requisições HTTP e retornar respostas, geralmente no formato JSON.
- *
- * @RequestMapping(value = "/users"): Define o caminho base (base path) para os endpoints da classe. Todas as requisições HTTP 
- * que começarem com "/users" serão direcionadas para os métodos dessa classe.
- * 
- * @GetMapping: Mapeia um método de um controlador para uma requisição HTTP GET. Ou seja, quando uma requisição HTTP GET for 
- * feita para o endpoint especificado, esse método será executado.
- */
+*
+* @RequestMapping(value = "/users"): Define o caminho base (base path) para os endpoints da classe. Todas as requisições HTTP 
+* que começarem com "/users" serão direcionadas para os métodos dessa classe.
+* 
+* @GetMapping: Mapeia um método de um controlador para uma requisição HTTP GET. Ou seja, quando uma requisição HTTP GET for 
+* feita para o endpoint especificado, esse método será executado.
+*/
 
 @RestController
 @RequestMapping(value = "/users")
